81872d64f5e36ff395a6b9ca65d5249a9fa1bf94
jRydell <johannilsrydell89@gmail.com>
2024-04-08 14:06:11 +0200

    added progressBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Form from "./Components/Form/Form";
 import Header from "./Components/Header/Header";
+import ProgressBar from "./Components/ProgressBar/ProgressBar";
 
 function App() {
   return (
     <>
       <Header />
       <Form />
+      <ProgressBar title="Progress" percentage={75} color={"green"} />
     </>
   );
 }
