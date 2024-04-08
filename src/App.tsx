729487d78b6748e78c3f3d13ce14ad35a0391adc
Bonnie <moller.bon@gmail.com>
2024-04-08 14:36:15 +0200

    checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
+import Checkbox from "./Components/Checkbox/Checkbox";
 import Form from "./Components/Form/Form";
 import Header from "./Components/Header/Header";
 import ProgressBar from "./Components/ProgressBar/ProgressBar";
 
+const names = [
+  "Alice",
+  "Bob",
+  "Charlie",
+  "David",
+  "Emma",
+  "Frank",
+  "Grace",
+  "Henry",
+  "Isabella",
+  "Jack",
+  "Kate",
+  "Liam",
+  "Mia",
+  "Noah"
+];
 function App() {
   return (
     <>
       <Header />
       <Form />
       <ProgressBar title="Progress" percentage={75} color={"green"} />
+      <h2>Names:</h2>
+      {names.map((name, index) => (
+        <Checkbox key={index} title={name} big={false}/>
+      ))}
     </>
   );
 }
