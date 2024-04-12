83af0fcb8e727cc98c424ba0800c095bb7849966
93mcqmil <mcqueen_1993@hotmail.com>
2024-04-12 16:53:44 +0200

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import Box from "./Components/Box/Box";
 // import Checkbox from "./Components/Checkbox/Checkbox";
 // import Form from "./Components/Form/Form";
 // import Header from "./Components/Header/Header";
-import Navbar from "./Components/Navbar/NavBar";
+// import Navbar from "./Components/Navbar/NavBar";
 // import ProgressBar from "./Components/ProgressBar/ProgressBar";
-import ShareButton from "./Components/ShareButton/ShareButton";
+// import ShareButton from "./Components/ShareButton/ShareButton";
 
 import Carousel from "./Components/Carousel/Carousel";
 import { myObj } from "./Components/Carousel/carouselTypes";
@@ -20,25 +20,25 @@ import ProgressBar from "./Components/ProgressBar/ProgressBar";
 import RadioButton from "./RadioButton/RadioButton";
 
 //-------------------------------------------NavBar-----------------------------------------------//
-const GoToNavLink = [
-  { Text: 'Home', href: '/#' },
-  { Text: 'About oss', href: '/#' },
-  { Text: 'Contakt oss', href: '/#' },
-  { Text: 'Different services', href: '/#' },
-  { Text: 'Cooperate with oss', href: '/#' }
-];
+// const GoToNavLink = [
+//   { Text: "Home", href: "/#" },
+//   { Text: "About oss", href: "/#" },
+//   { Text: "Contakt oss", href: "/#" },
+//   { Text: "Different services", href: "/#" },
+//   { Text: "Cooperate with oss", href: "/#" },
+// ];
 
 //------------------------------------------------------------------------------------------//
 
 //-------------------------------------Social Media Butten's---------------------------------------//
 
-const socialLinks = [
-  { className: "twitter", icon: "ri-twitter-line", href: "#" },
-  { className: "facebook", icon: "ri-facebook-line", href: "#" },
-  { className: "close", icon: "ri-close-line", href: "#" }, // För att stänga knap menyn 
-  { className: "discord", icon: "ri-discord-line", href: "#" },
-  { className: "whatsapp", icon: "ri-whatsapp-line", href: "#" }
-];
+// const socialLinks = [
+//   { className: "twitter", icon: "ri-twitter-line", href: "#" },
+//   { className: "facebook", icon: "ri-facebook-line", href: "#" },
+//   { className: "close", icon: "ri-close-line", href: "#" }, // För att stänga knap menyn
+//   { className: "discord", icon: "ri-discord-line", href: "#" },
+//   { className: "whatsapp", icon: "ri-whatsapp-line", href: "#" },
+// ];
 
 //------------------------------------------------------------------------------------------//
 
@@ -61,7 +61,7 @@ const socialLinks = [
 function App() {
   return (
     <>
-      <Navbar GoToNavLink={GoToNavLink} />
+      {/* <Navbar GoToNavLink={GoToNavLink} /> */}
       {/* <Header />
       <Form />
       
@@ -78,11 +78,8 @@ function App() {
     */}
       <RadioButton />
       <Box />
-
-      <ShareButton socialLinks={socialLinks} />
-=======
+      {/* <ShareButton socialLinks={socialLinks} /> */}
       <MixComponent />
-
     </>
   );
 }
